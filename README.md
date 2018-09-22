56b74337afc5c1ff7e708a8c37394dc524b90f55
Kristina Macekovic <kmacekovic@gmail.com>
2018-09-22 21:30:12 +0200

    add all

---
+++ README.md
@@ -0,0 +1,16 @@
+# React Chat App
+
+Quick start:
+
+```
+$ npm install
+$ npm run build
+````
+
+## Development
+
+Run Webpack in watch-mode to continually compile the JavaScript as you work:
+
+```
+$ npm run watch
+```
---
+++ config.js
@@ -0,0 +1,4 @@
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/c08d6b44-abd3-43bd-bece-937ce0d486f7/token";
+const instanceLocator = "v1:us1:c08d6b44-abd3-43bd-bece-937ce0d486f7";
+
+export { tokenUrl, instanceLocator }
